c3869a993293ef56c20ac9a3d96771c1cab6d472
Andriy Goloviy <kykyagoloviy@gmail.com>
2025-07-20 12:43:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,35 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig(({ command }) => ({
-  root: 'src',
-  base: command === 'serve' ? '' : '/goit-js-hw-10/',
-  server: {
-    fs: {
-      allow: ['..']
-    }
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index:    resolve(__dirname, 'src/index.html'),
-        timer:    resolve(__dirname, 'src/1-timer.html'),
-        snackbar: resolve(__dirname, 'src/2-snackbar.html')
-      },
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) return 'vendor';
-        },
-        entryFileNames: '[name].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  return {
+    root: 'src',
+    base: isDev ? '' : '/goit-js-hw-10/',
+    server: {
+      fs: {
+        allow: ['..']
       }
-    }
-  },
-  plugins: [
-   
-  ]
-}));
+    },
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/index.html'),
+          timer: resolve(__dirname, 'src/1-timer.html'),
+          snackbar: resolve(__dirname, 'src/2-snackbar.html')
+        },
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) return 'vendor';
+          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]'
+        }
+      }
+    },
+    plugins: []
+  };
+});
